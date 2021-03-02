6737cfe96efd741339d5fcca12fbba80c41243d0
xeusteerapat <xeus085@gmail.com>
2021-03-02 21:54:09 +0700

    [ADD] :sparkles: add new question page

--- pages/question.js
+++ pages/question.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Box,
+  Input,
+  Container,
+  Textarea,
+  Button,
+  Heading,
+} from '@chakra-ui/react';
+import { useAuth } from '../lib/useSupabase';
 
 const question = () => {
   return (
-    <div>
-      <h1>Create new question</h1>
-    </div>
+    <Container>
+      <Box>
+        <Heading>Add New Question</Heading>
+        <form action=''>
+          <FormControl id='title'>
+            <FormLabel>Question Title</FormLabel>
+            <Input type='text' />
+            <FormLabel>Details</FormLabel>
+            <Textarea size='lg' />
+          </FormControl>
+          <Button>Submit</Button>
+        </form>
+      </Box>
+    </Container>
   );
 };
 
